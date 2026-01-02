c3d0dfdde2934a3a631e60ad30a1a99ab05b4c16
박지훈 <jihun6548@naver.com>
2026-01-02 15:46:06 +0900

    chore: SwaggerConfig 매직넘버 상수화 #2

--- src/main/java/cmc/delta/global/config/swagger/SwaggerConfig.java
+++ src/main/java/cmc/delta/global/config/swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package cmc.delta.global.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -11,29 +12,48 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+  private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+  private static final String BEARER_SCHEME = "bearer";
+  private static final String BEARER_FORMAT_JWT = "JWT";
+
+  private static final String GROUP_ALL = "all";
+  private static final String DISPLAY_NAME_ALL = "All API";
+  private static final String PATHS_MATCH_ALL = "/**";
+
+  private static final String API_TITLE_ALL = "모든 API";
+  private static final String API_VERSION = "v0.4";
+
   private SecurityScheme createBearerAuthScheme() {
     return new SecurityScheme()
-        .type(SecurityScheme.Type.HTTP)
-        .bearerFormat("JWT")
-        .scheme("bearer");
+            .type(SecurityScheme.Type.HTTP)
+            .bearerFormat(BEARER_FORMAT_JWT)
+            .scheme(BEARER_SCHEME);
   }
 
   private OpenApiCustomizer createOpenApiCustomizer(String title, String version) {
     return openApi -> {
       openApi.info(new Info().title(title).version(version));
-      openApi.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
-      openApi.schemaRequirement("bearerAuth", createBearerAuthScheme());
+
+      openApi.addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+
+      Components components = openApi.getComponents();
+      if (components == null) {
+        components = new Components();
+        openApi.setComponents(components);
+      }
+      components.addSecuritySchemes(SECURITY_SCHEME_NAME, createBearerAuthScheme());
     };
   }
 
   @Bean
   public GroupedOpenApi allApi(ApiErrorCodeOperationCustomizer apiErrorCodeOperationCustomizer) {
     return GroupedOpenApi.builder()
-        .group("all")
-        .pathsToMatch("/**")
-        .displayName("All API")
-        .addOpenApiCustomizer(createOpenApiCustomizer("모든 API", "v0.4"))
-        .addOperationCustomizer(apiErrorCodeOperationCustomizer)
-        .build();
+            .group(GROUP_ALL)
+            .pathsToMatch(PATHS_MATCH_ALL)
+            .displayName(DISPLAY_NAME_ALL)
+            .addOpenApiCustomizer(createOpenApiCustomizer(API_TITLE_ALL, API_VERSION))
+            .addOperationCustomizer(apiErrorCodeOperationCustomizer)
+            .build();
   }
 }
