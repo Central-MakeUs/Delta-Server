1df87c90929f78c0013806b4deac8e6f07629928
박지훈 <jihun6548@naver.com>
2026-01-13 21:39:21 +0900

    chore: global persistence BaseEntity 추가 설정 #20

---
+++ src/main/java/cmc/delta/global/persistence/BaseCreatedEntity.java
@@ -0,0 +1,20 @@
+// src/main/java/cmc/delta/global/persistence/BaseCreatedEntity.java
+package cmc.delta.global.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseCreatedEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+}
