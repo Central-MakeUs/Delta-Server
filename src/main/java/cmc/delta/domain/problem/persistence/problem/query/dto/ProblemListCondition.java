2f5071bf5e9eed5489a7be9e3adc7b6861f7284c
박지훈 <jihun6548@naver.com>
2026-01-19 23:37:07 +0900

    feat: ProblemFilter 추가 및 Dto 설정 #26

--- src/main/java/cmc/delta/domain/problem/persistence/problem/query/dto/ProblemListCondition.java
+++ src/main/java/cmc/delta/domain/problem/persistence/problem/query/dto/ProblemListCondition.java
@@ -1,10 +1,12 @@
 package cmc.delta.domain.problem.persistence.problem.query.dto;
 
 import cmc.delta.domain.problem.api.problem.dto.request.ProblemListSort;
+import cmc.delta.domain.problem.model.enums.ProblemStatusFilter;
 
 public record ProblemListCondition(
 	String subjectId,
 	String unitId,
 	String typeId,
-	ProblemListSort sort
+	ProblemListSort sort,
+	ProblemStatusFilter status
 ) { }
