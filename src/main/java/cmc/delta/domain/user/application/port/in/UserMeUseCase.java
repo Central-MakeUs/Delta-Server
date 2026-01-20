7271875b2e4ba47484f087833e6e43cf10bd392c
박지훈 <jihun6548@naver.com>
2026-01-20 15:32:53 +0900

    feat: User UseCase추가 #28

---
+++ src/main/java/cmc/delta/domain/user/application/port/in/UserMeUseCase.java
@@ -0,0 +1,4 @@
+package cmc.delta.domain.user.application.port.in;
+
+public class UserMeUseCase {
+}
---
+++ src/main/java/cmc/delta/domain/user/application/port/out/UserRepositoryPort.java
@@ -0,0 +1,4 @@
+package cmc.delta.domain.user.application.port.out;
+
+public class UserRepositoryPort {
+}
