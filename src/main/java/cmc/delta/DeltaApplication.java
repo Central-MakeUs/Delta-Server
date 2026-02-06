5c9d9ba479a8e81f2ef606e438b65b5b95a82387
박지훈 <jihun6548@naver.com>
2026-02-06 23:49:51 +0900

    feat: 오래된 스캔 정리를 위한 purge 워커/스케줄러 인프라 추가 #52

--- src/main/java/cmc/delta/DeltaApplication.java
+++ src/main/java/cmc/delta/DeltaApplication.java
@@ -8,11 +8,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import cmc.delta.domain.problem.adapter.in.worker.properties.AiWorkerProperties;
 import cmc.delta.domain.problem.adapter.in.worker.properties.OcrWorkerProperties;
+import cmc.delta.domain.problem.adapter.in.worker.properties.PurgeWorkerProperties;
 import cmc.delta.global.config.FrontendProperties;
 
 @EnableScheduling
 @EnableJpaAuditing
-@EnableConfigurationProperties({OcrWorkerProperties.class, AiWorkerProperties.class, FrontendProperties.class})
+@EnableConfigurationProperties({OcrWorkerProperties.class, AiWorkerProperties.class, PurgeWorkerProperties.class,
+	FrontendProperties.class})
 @SpringBootApplication
 public class DeltaApplication {
 
