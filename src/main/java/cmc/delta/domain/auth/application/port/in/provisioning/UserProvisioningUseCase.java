f16c819f4a72013249c3ebb26ea55907805ff9c7
2jiyong <jdragon2000@naver.com>
2026-05-06 17:43:59 +0900

    feat: UserProvisioning에 userRole 추가

--- src/main/java/cmc/delta/domain/auth/application/port/in/provisioning/UserProvisioningUseCase.java
+++ src/main/java/cmc/delta/domain/auth/application/port/in/provisioning/UserProvisioningUseCase.java
@@ -1,8 +1,10 @@
 package cmc.delta.domain.auth.application.port.in.provisioning;
 
+import cmc.delta.domain.user.model.enums.UserRole;
+
 public interface UserProvisioningUseCase {
 	ProvisioningResult provisionSocialUser(SocialUserProvisionCommand command);
 
-	record ProvisioningResult(long userId, String email, String nickname, boolean isNewUser) {
+	record ProvisioningResult(long userId, String email, String nickname, UserRole role, boolean isNewUser) {
 	}
 }
